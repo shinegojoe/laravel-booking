820e0a7e6eefd6ce42b292b32df534b26fedc246
shinegojoe <shinegojoe@gmail.com>
2024-01-02 13:19:22 +0800

    add default statusCode

--- app/Utils/appResponse.php
+++ app/Utils/appResponse.php
@@ -20,19 +20,19 @@ class ErrorInfo {
 class AppResponse {
 
 
-    public static function errorResp(int $code, string $detail) {
+    public static function errorResp(int $code, string $detail, int $statusCode=200) {
         $error = new ErrorInfo($code, $detail);
         $message = "failed";
-        return response()->json(["message"=> $message,"error"=> $error]);
+        return response()->json(["message"=> $message,"error"=> $error], $statusCode);
 
     }
 
-    public static function successResp($data) {
+    public static function successResp($data, int $statusCode= 200) {
         $newData = array();
         foreach ($data->toArray() as $key => $value) {
             $newKey = Str::camel($key);
             $newData[$newKey] = $value;
         }
-        return response()->json(["message"=> "success","data"=> $newData]);
+        return response()->json(["message"=> "success","data"=> $newData], $statusCode);
     }
 }
